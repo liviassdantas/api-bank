4002f5488a56f0bfec04a8ef25984e8e9b39aae1
Livia Dantas <livia.ss.dantas@gmail.com>
2021-03-28 18:04:16 -0300

    auth config completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {MongooseModule} from '@nestjs/mongoose'
 import {ConfigModule} from '@nestjs/config'
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ConfigModule.forRoot(),
-    MongooseModule.forRoot('mongodb://db_mongo/api-bank')],
+    MongooseModule.forRoot('mongodb://db_mongo/api-bank'),
+    AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
