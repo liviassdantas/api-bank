cbbcc65b2e15f13602052826d1fd471fbbf461a1
Livia Dantas <livia.ss.dantas@gmail.com>
2021-03-28 18:50:07 -0300

    swagger configuration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, UseGuards, Request, Post } from '@nestjs/common';
+import { ApiOkResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 
@@ -10,7 +11,10 @@ constructor(
 ){}
 
   @UseGuards(LocalAuthGuard)
+  @ApiTags('auth')
   @Post('auth/login')
+  @ApiOkResponse({description: "access-token: a bearer token"})
+  @ApiUnauthorizedResponse({description: "You have no permission to this action."})
   async login(@Request() req: any){
     return this.authService.login(req.user);
   }
