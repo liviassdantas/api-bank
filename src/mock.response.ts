fd874167f9b04846b28d2b3035b18279af0cc63e
Livia Dantas <livia.ss.dantas@gmail.com>
2021-03-28 20:47:26 -0300

    auth completed

---
+++ src/mock.response.ts
@@ -0,0 +1,5 @@
+export default function mockToken(){
+    return {
+            "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MTY5NjY4NjgsImV4cCI6MTYxNjk2NjkyOH0.b2eefmnnwc0SN8ZDFy4OB8HwtiYcgAG72sHB6DWwhL0"
+    }
+}
