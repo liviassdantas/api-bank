d5d18de9c43688111b914ba915c12767a2b61258
Livia Dantas <livia.ss.dantas@gmail.com>
2021-04-03 18:30:15 -0300

    transfers module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,21 @@
+import { TransfersController } from './transfers/transfers.controller';
+import { TransfersService } from './transfers/transfers.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {MongooseModule} from '@nestjs/mongoose'
-import {ConfigModule} from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { AccountModule } from './accounts/accounts.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(),
-            MongooseModule.forRoot('mongodb://localhost:27017/api-bank'),
-            AuthModule,
-            AccountModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost:27017/api-bank'),
+    AuthModule,
+    AccountModule,
+  ],
+  controllers: [TransfersController, AppController],
+  providers: [TransfersService, AppService],
 })
 export class AppModule {}
